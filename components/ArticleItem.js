5f424642665c931a2e0a33906f43f7ecda9ecab1
ludo loops <ludoloops@protonmail.com>
2021-06-09 23:37:30 +0200

    Feat: static artcile up to 6, then fetch them

--- components/ArticleItem.js
+++ components/ArticleItem.js
@@ -3,9 +3,9 @@ import Link from 'next/link'
 
 const ArticleItem = ({ article }) => {
   return (
-    <Link href='/article/[id]' as={`/article/${article.id}`}>
+    <Link href={`/article/${article.id}`}>
       <a className={articleStyles.card}>
-        <h3>{article.title} &arr;</h3>
+        <h3>{article.title} &rarr;</h3>
         <p>{article.title}</p>
       </a>
     </Link>
