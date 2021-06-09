20cdbc3198150c8c8c64001c048e1efccfddf1fa
ludo loops <ludoloops@protonmail.com>
2021-06-09 23:59:28 +0200

    Feat: get article from own API

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import { server } from '../config'
+
 import Head from 'next/head'
 import ArticleList from '../components/ArticleList'
 
@@ -14,7 +16,8 @@ export default function Home({ articles }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+  const res = await fetch(`${server}/api/articles`)
+
   const articles = await res.json()
 
   return {
@@ -23,3 +26,13 @@ export const getStaticProps = async () => {
     },
   }
 }
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=6`)
+//   const articles = await res.json()
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   }
+// }
